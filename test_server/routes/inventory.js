e2f410bc16a5993ae94589c36946239a334ec4c8
MrHello009 <felix.mikaelsson@elev.ga.ntig.se>
2026-04-29 13:47:28 +0200

    update

--- test_server/routes/inventory.js
+++ test_server/routes/inventory.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 const router = express.Router();
-const verifyToken = require(`../middleware/auth.js`);
+const { verifyToken } = require(`../middleware/auth.js`);
 const InventoryController = require(`../controllers/inventorycontroller.js`);
 
 router.get(`/`, verifyToken, InventoryController.getInventory);
--- test_server/routes/saves.js
+++ test_server/routes/saves.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 const router = express.Router();
-const verifyToken = require(`../middleware/auth.js`);
+const { verifyToken } = require(`../middleware/auth.js`);
 const { saveGameData, loadGameData } = require(`../controllers/savescontroller.js`);
 
 
